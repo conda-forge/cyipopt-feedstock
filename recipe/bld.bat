db797f32878ebdb41ac1f65cdd9274e7ab124bf8
Jason K. Moore <moorepants@gmail.com>
2024-10-01 17:15:22 +0200

    Adde --no-build-isolation to pip command.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
 set IPOPTWINDIR=USECONDAFORGEIPOPT
-%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+%PYTHON% -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vvv
