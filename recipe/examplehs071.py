6a48f561bee5e40b5f1c99c0e9ddc14c1e9ec700
Jason K. Moore <moorepants@gmail.com>
2024-10-01 17:49:40 +0200

    Update the hs071 example to match cyipopt.

--- recipe/examplehs071.py
+++ recipe/examplehs071.py
@@ -1,52 +1,74 @@
 # -*- coding: utf-8 -*-
 """
-cyipot: Python wrapper for the Ipopt optimization package, written in Cython.
+cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
 
-Copyright (C) 2012 Amit Aides
-Author: Amit Aides <amitibo@tx.technion.ac.il>
-URL: <http://http://code.google.com/p/cyipopt/>
-License: EPL 1.0
+Copyright (C) 2012-2015 Amit Aides
+Copyright (C) 2015-2017 Matthias Kümmerer
+Copyright (C) 2017-2024 cyipopt developers
+
+License: EPL 2.0
 """
-#
+
 # Test the "ipopt" Python interface on the Hock & Schittkowski test problem
 # #71. See: Willi Hock and Klaus Schittkowski. (1981) Test Examples for
 # Nonlinear Programming Codes. Lecture Notes in Economics and Mathematical
 # Systems Vol. 187, Springer-Verlag.
 #
 # Based on matlab code by Peter Carbonetto.
-#
 
 import numpy as np
-import scipy.sparse as sps
 import cyipopt
 
 
-class HS071(object):
+class hs071:
 
     def __init__(self):
-        self.hs = sps.coo_matrix(np.tril(np.ones((4, 4))))
+        pass
 
     def objective(self, x):
-        return x[0]*x[3]*np.sum(x[0:3]) + x[2]
+        #
+        # The callback for calculating the objective
+        #
+        return x[0] * x[3] * np.sum(x[0:3]) + x[2]
 
     def gradient(self, x):
+        #
+        # The callback for calculating the gradient
+        #
         return np.array([
-                    x[0]*x[3] + x[3]*np.sum(x[0:3]),
-                    x[0]*x[3],
-                    x[0]*x[3] + 1.0,
-                    x[0]*np.sum(x[0:3])
+                    x[0] * x[3] + x[3] * np.sum(x[0:3]),
+                    x[0] * x[3],
+                    x[0] * x[3] + 1.0,
+                    x[0] * np.sum(x[0:3])
                     ])
 
     def constraints(self, x):
+        #
+        # The callback for calculating the constraints
+        #
         return np.array((np.prod(x), np.dot(x, x)))
 
     def jacobian(self, x):
+        #
+        # The callback for calculating the Jacobian
+        #
         return np.concatenate((np.prod(x) / x, 2*x))
 
     def hessianstructure(self):
-        return (self.hs.col, self.hs.row)
+        #
+        # The structure of the Hessian
+        # Note:
+        # The default hessian structure is of a lower triangular matrix. Therefore
+        # this function is redundant. I include it as an example for structure
+        # callback.
+        #
+
+        return np.nonzero(np.tril(np.ones((4, 4))))
 
     def hessian(self, x, lagrange, obj_factor):
+        #
+        # The callback for calculating the Hessian
+        #
         H = obj_factor*np.array((
                 (2*x[3], 0, 0, 0),
                 (x[3],   0, 0, 0),
@@ -61,17 +83,35 @@ class HS071(object):
 
         H += lagrange[1]*2*np.eye(4)
 
-        return H[self.hs.row, self.hs.col]
+        row, col = self.hessianstructure()
 
-    def intermediate(self, alg_mod, iter_count, obj_value, inf_pr, inf_du, mu,
-                     d_norm, regularization_size, alpha_du, alpha_pr,
-                     ls_trials):
+        return H[row, col]
 
-        msg = "Objective value at iteration #{:d} is - {:g}"
-        print(msg.format(iter_count, obj_value))
+    def intermediate(
+            self,
+            alg_mod,
+            iter_count,
+            obj_value,
+            inf_pr,
+            inf_du,
+            mu,
+            d_norm,
+            regularization_size,
+            alpha_du,
+            alpha_pr,
+            ls_trials
+            ):
+
+        #
+        # Example for the use of the intermediate callback.
+        #
+        print("Objective value at iteration #%d is - %g" % (iter_count, obj_value))
 
 
 def main():
+    #
+    # Define the problem
+    #
     x0 = [1.0, 5.0, 5.0, 1.0]
 
     lb = [1.0, 1.0, 1.0, 1.0]
@@ -80,25 +120,42 @@ def main():
     cl = [25.0, 40.0]
     cu = [2.0e19, 40.0]
 
-    nlp = cyipopt.Problem(n=len(x0), m=len(cl), problem_obj=HS071(), lb=lb,
-                          ub=ub, cl=cl, cu=cu)
+    nlp = cyipopt.Problem(
+        n=len(x0),
+        m=len(cl),
+        problem_obj=hs071(),
+        lb=lb,
+        ub=ub,
+        cl=cl,
+        cu=cu
+        )
 
-    nlp.addOption(b'mu_strategy', b'adaptive')
-    nlp.addOption(b'tol', 1e-7)
+    #
+    # Set solver options
+    #
+    #nlp.addOption('derivative_test', 'second-order')
+    nlp.add_option('mu_strategy', 'adaptive')
+    nlp.add_option('tol', 1e-7)
 
-    nlp.setProblemScaling(obj_scaling=2, x_scaling=[1, 1, 1, 1])
-    nlp.addOption(b'nlp_scaling_method', b'user-scaling')
+    #
+    # Scale the problem (Just for demonstration purposes)
+    #
+    nlp.set_problem_scaling(
+        obj_scaling=2,
+        x_scaling=[1, 1, 1, 1]
+        )
+    nlp.add_option('nlp_scaling_method', 'user-scaling')
 
+    #
+    # Solve the problem
+    #
     x, info = nlp.solve(x0)
 
-    msg = "Solution of the primal variables: x={:s}\n"
-    print(msg.format(repr(x)))
+    print("Solution of the primal variables: x=%s\n" % repr(x))
 
-    msg = "Solution of the dual variables: lambda={:s}\n"
-    print(msg.format(repr(info['mult_g'])))
+    print("Solution of the dual variables: lambda=%s\n" % repr(info['mult_g']))
 
-    msg = "Objective={:s}\n"
-    print(msg.format(repr(info['obj_val'])))
+    print("Objective=%s\n" % repr(info['obj_val']))
 
 
 if __name__ == '__main__':
